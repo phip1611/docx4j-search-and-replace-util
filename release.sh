d721a0dabd5f97f3c9b694046b81be58a6b699c3
Philipp Schuster <phip1611@gmail.com>
2021-12-11 19:26:04 +0100

    release script: build fix

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
 mvn clean
 
+# https://issues.sonatype.org/browse/OSSRH-66257
+export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
+
 mvn release:prepare
 
 mvn release:perform
