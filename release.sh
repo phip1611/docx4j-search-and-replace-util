a8efe2bd18d37ccbd7fa32b064834186ba865b12
Philipp Schuster <phip1611@gmail.com>
2023-11-04 12:29:01 +0100

    util: update release.sh

--- release.sh
+++ release.sh
@@ -7,17 +7,11 @@ set -x
 
 mvn clean
 
-# https://issues.sonatype.org/browse/OSSRH-66257
-export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
-
-mvn release:prepare
-
-mvn verify gpg:sign
-
+# Actually the profile should be invoked automatically, but apparentely, I
+# faced situations where the artifacts didn't get signed. So, better be
+# explicit here.
+mvn -Prelease-sign-artifacts release:prepare
 mvn release:perform
-
-rm release.properties
-rm pom.xml.releaseBackup
 
 git push --tags
 git push origin main
