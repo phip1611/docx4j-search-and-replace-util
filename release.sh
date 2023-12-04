91f16d1509b16931a43619243ea52ee050fe9776
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-12-04 09:15:40 +0100

    typo

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ set -x
 
 mvn clean
 
-# Actually the profile should be invoked automatically, but apparentely, I
+# Actually the profile should be invoked automatically, but apparently, I
 # faced situations where the artifacts didn't get signed. So, better be
 # explicit here.
 mvn -Prelease-sign-artifacts release:prepare
